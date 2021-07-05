2cd5a3b37e6dd94d8c43e3a4e0c076c91c7c5a56
Erich(Renyong) Wang <eriwan@microsoft.com>
2021-07-05 11:03:49 +0800

    firstcommit

---
+++ TestHttpModule/TestHttpModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Management.Automation;
+using System.Net.Http;
+
+namespace TestHttpModule
+{
+    [Cmdlet("Test", "HttpModule")]
+    public class TestHttpModuleCommand : Cmdlet
+    {
+        protected override void EndProcessing()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://google.com");
+            var result = client.SendAsync(request).Result;
+            WriteWarning(result.RequestMessage.ToString());
+        }
+    }
+}
---
+++ TestHttpModule/TestHttpModule.psd1
@@ -0,0 +1,7 @@
+﻿@{
+    Author = 'Me'
+    ModuleVersion = '0.0.1'
+    RootModule = 'TestHttpModule.dll'
+    CmdletsToExport = @('Test-HttpModule')
+    PowerShellVersion = '7.0'
+}
